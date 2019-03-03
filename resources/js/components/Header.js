5e8a2fb3cc8e7ccefe88b3b7a6f977b112993347
umar frog <omar.hambaran@frogasia.com>
2019-03-03 18:18:21 +0800

    fixed-top navbar

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
+  <nav className='navbar navbar-expand-md navbar-light navbar-laravel fixed-top'>
     <div className='container'>
       <Link className='navbar-brand' to='/'>Book Meeting Room</Link>
     </div>
